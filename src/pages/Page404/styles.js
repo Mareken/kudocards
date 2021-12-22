5c8a2baaaa815b7cfda8c489004235595e453b65
Gabriel Antonio <gabriel.souza@easynvest.com.br>
2021-12-22 13:36:17 -0300

    Adjust theming

--- src/pages/Page404/styles.js
+++ src/pages/Page404/styles.js
@@ -7,6 +7,7 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  background: ${props => props.theme.colors.background};
 `;
 
 export const Text = styled.p`
